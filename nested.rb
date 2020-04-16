9d93b43e120bede9b603e995b7e26559cc158431
Regina Furness <skritschy@gmail.com>
2020-04-16 23:48:32 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -82,8 +82,8 @@ def adding_matz
           :languages => ["C"]
         }
      }
-programmer_hash[:yukihiro_matsumoto] = :known_for{"Ruby"}
-programmer_hash[:yukihiro_matsumoto][:languages] = ""
+programmer_hash[:yukihiro_matsumoto] = {:known_for => "RUBY", :languages => ["LISP", "C"]
+programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP", "C"]
 
  
  programmer_hash
