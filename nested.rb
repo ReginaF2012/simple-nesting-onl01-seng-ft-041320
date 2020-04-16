dcfe5cdbb7a47d21a7cdf0995cf6b6b7caf26ab5
Regina Furness <skritschy@gmail.com>
2020-04-16 23:45:56 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -15,8 +15,7 @@ def hopper
           :languages => ["C"]
         }
      }
-
-
+programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
@@ -37,6 +36,8 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     
+     programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +56,8 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     
+    programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
@@ -79,7 +82,10 @@ def adding_matz
           :languages => ["C"]
         }
      }
+programmer_hash[:yukihiro_matsumoto] = :known_for{}
 
+ 
+ programmer_hash
     
 end
 
